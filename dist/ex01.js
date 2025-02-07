f46617f5bdbcdcf8d1a9a0ef52aedf5157178849
koya <koya9431ptr@gmail.com>
2025-02-07 17:41:13 +0000

    done

---
+++ dist/ex01.js
@@ -0,0 +1,8 @@
+"use strict";
+// Create a function called addNumbers.
+// The function should take two parameters, both of type number.
+// It should return the sum of the two numbers as a number.
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+console.log(addNumbers(3, 7)); // Expected output: 10
---
+++ dist/ex03.js
@@ -0,0 +1,10 @@
+"use strict";
+// Create a function called combine that can accept two numbers OR two strings.
+// Use function overloading to define two versions of this function.
+// If two numbers are passed, return their sum.
+// If two strings are passed, return their concatenated (combined) value.
+function combine(a, b) {
+    return a + b;
+}
+console.log(combine(10, 20)); // Expected output: 30
+console.log(combine("Hello, ", "World!")); // Expected output: Hello, World!
